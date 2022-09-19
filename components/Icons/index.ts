52c2ba93e7df6f2c721336b0015a4d6c131249ca
Neesh <avaneeshsamsi@gmail.com>
2022-09-19 14:47:22 +0530

    feat: Add named export for all icons

---
+++ components/Icons/index.ts
@@ -0,0 +1,5 @@
+export { default as RightArrow } from "./RightArrow";
+export { default as Chat } from "./Chat";
+export { default as YouTube } from "./YouTube";
+export { default as Instagram } from "./Instagram";
+export { default as Facebook } from "./Facebook";
