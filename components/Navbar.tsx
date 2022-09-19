0509b80e0144486619be0fa470988916babd2de0
Neesh <avaneeshsamsi@gmail.com>
2022-09-19 16:10:03 +0530

    feat: Responsive upto 3xl +

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 right-0 flex items-center justify-between px-32 py-8 xl:px-56 2xl:px-80">
+    <nav className="fixed top-0 left-0 right-0 flex items-center justify-between px-32 py-8 xl:px-44 2xl:px-56 3xl:mx-auto 3xl:max-w-[144rem] 3xl:px-0">
       <h2 className="text-2xl font-semibold lg:text-3xl 2xl:text-4xl">
         Shravan Samsi
       </h2>
