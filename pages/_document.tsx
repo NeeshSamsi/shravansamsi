a69a7d2e8e89682c01959532a9f613cb4ed0c327
Neesh <avaneeshsamsi@gmail.com>
2022-09-19 17:19:16 +0530

    fix: React compatible html attributes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document"
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <meta charSet="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="author" content="Shravan Samsi" />
         <link rel="icon" href="/favicon.ico" />
@@ -17,10 +17,10 @@ export default function Document() {
         />
       </Head>
 
-      <body className="bg-dark text-light font-sans">
+      <body className="bg-dark font-sans text-light">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
