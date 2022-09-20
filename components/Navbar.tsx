edc676d2df373c75093d6a96b855d8797c55e4ba
Neesh <avaneeshsamsi@gmail.com>
2022-09-20 14:28:29 +0530

    feat: Add highlight for active link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import { Facebook, Instagram, YouTube } from "./Icons";
 
@@ -9,6 +10,9 @@ export default function Navbar() {
     setIsHamburgerOpen((prevState) => !prevState);
   };
 
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
   return (
     <nav className="sticky top-0 left-0 right-0 flex items-center justify-between bg-dark py-8 px-4 sm:px-8 md:px-12 lg:px-32 xl:px-44 2xl:px-56 3xl:mx-auto 3xl:max-w-[144rem] 3xl:px-0">
       <h2 className="z-10 text-3xl font-semibold lg:text-2xl xl:text-3xl 2xl:text-4xl">
@@ -18,19 +22,49 @@ export default function Navbar() {
       {/* Desktop Nav Links - hidden, flex from lg + */}
       <div className="hidden gap-4 font-light lg:flex lg:gap-6 lg:text-base xl:text-lg 2xl:gap-8 2xl:text-2xl">
         <Link href="/">
-          <a>Home</a>
+          <a
+            className={`transition-all hover:text-accent ${
+              currentRoute === "/" && "font-medium text-accent"
+            }`}
+          >
+            Home
+          </a>
         </Link>
         <Link href="/about">
-          <a>About</a>
+          <a
+            className={`transition-all hover:text-accent ${
+              currentRoute === "/about" && "font-medium text-accent"
+            }`}
+          >
+            About
+          </a>
         </Link>
         <Link href="/events">
-          <a>Events</a>
+          <a
+            className={`transition-all hover:text-accent ${
+              currentRoute === "/events" && "font-medium text-accent"
+            }`}
+          >
+            Events
+          </a>
         </Link>
         <Link href="/gallery">
-          <a>Gallery</a>
+          <a
+            className={`transition-all hover:text-accent ${
+              currentRoute === "/gallery" && "font-medium text-accent"
+            }`}
+          >
+            Gallery
+          </a>
         </Link>
         <Link href="/contact">
-          <a>Contact</a>
+          <a
+            className={`transition-all hover:text-accent ${
+              currentRoute === "/contact" && "font-medium text-accent"
+            }`}
+          >
+            Contact
+          </a>
         </Link>
       </div>
 
