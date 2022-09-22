a94f559451bae25876ccd80d6de67384bf6b7d87
Neesh <avaneeshsamsi@gmail.com>
2022-09-22 13:48:58 +0530

    feat: Init prismic client, Add typed Event Data

---
+++ lib/prismicio.ts
@@ -0,0 +1,31 @@
+import * as prismic from "@prismicio/client";
+
+const repoName = "shravan";
+const endpoint = prismic.getEndpoint(repoName);
+const client = prismic.createClient(endpoint);
+
+export interface EventType {
+  id: string;
+  type: string;
+  href: string;
+  tags: string[];
+  first_publication_date: string;
+  last_publication_date: string;
+  data: {
+    name: string;
+    location: {
+      name: string;
+      url: string;
+    };
+    date: string;
+    time: string;
+    paid: boolean;
+    ticket_link: string;
+    poster: {
+      url: string;
+      alt: string;
+    };
+  };
+}
+
+export default client;
