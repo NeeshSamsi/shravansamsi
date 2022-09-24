e2abd6f239e588b2ef2cfb5d9c4b92c7c2e80217
Neesh <avaneeshsamsi@gmail.com>
2022-09-24 12:42:45 +0530

    feat: Add slideshow navigation icon

---
+++ components/Icons/CircleCaret.tsx
@@ -0,0 +1,25 @@
+export default function CircleCaret() {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="16"
+        cy="16"
+        r="11.25"
+        stroke="currentColor"
+        strokeWidth="1.5"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M14.2396 11.2312C14.5592 10.9229 15.0772 10.9229 15.3967 11.2312L19.7604 15.4418C20.0799 15.7501 20.0799 16.2499 19.7604 16.5582L15.3967 20.7688C15.0772 21.0771 14.5592 21.0771 14.2396 20.7688C13.9201 20.4605 13.9201 19.9606 14.2396 19.6523L18.0247 16L14.2396 12.3477C13.9201 12.0394 13.9201 11.5395 14.2396 11.2312Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
