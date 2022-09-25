2133a697ba60e9b82d0020c2fbfcb2b6c3660ff3
Neesh <avaneeshsamsi@gmail.com>
2022-09-25 15:07:27 +0530

    feat: Add icon to exit Dialog

---
+++ components/Icons/Cross.tsx
@@ -0,0 +1,24 @@
+export default function Cross() {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7.75048 24.2496L24.2496 7.75043"
+        stroke="currentColor"
+        stroke-width="2.5"
+        stroke-linecap="round"
+      />
+      <path
+        d="M7.75048 7.75041L24.2496 24.2496"
+        stroke="currentColor"
+        stroke-width="2.5"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+}
