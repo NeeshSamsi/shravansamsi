e94b6d4aa2c63b32bddd9b8f5fb652af66feb0e4
Neesh <avaneeshsamsi@gmail.com>
2023-01-04 22:25:08 +0530

    feat: Add Footer to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ import {
   Facebook,
 } from "../components/Icons";
 import HeadComponent from "../components/HeadComponent";
+import Footer from "../components/Footer";
 
 const Home: NextPage = () => {
   return (
@@ -95,6 +96,8 @@ const Home: NextPage = () => {
           </div>
         </div>
       </main>
+
+      <Footer />
     </>
   );
 };
