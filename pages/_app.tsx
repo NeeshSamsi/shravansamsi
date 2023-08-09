59d98e455d44e45ac0fa29b77c05287df6d841c7
Neesh <avaneeshsamsi@gmail.com>
2023-08-09 12:18:49 +0530

    feat: Add umami analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
@@ -8,6 +9,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
+      <Script
+        strategy="lazyOnload"
+        src="https://neesh-umami.vercel.app/script.js"
+        data-website-id="cc681e71-31cf-4b34-8101-c5926a8fef1f"
+      />
       <Component {...pageProps} />
     </>
   );
