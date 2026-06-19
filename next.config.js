97f6f4576950837b61d3db8f81e7221e64d0d854
Neesh <avaneeshsamsi@gmail.com>
2026-06-19 21:32:18 +0700

    feat: masterclass link redirect

--- next.config.js
+++ next.config.js
@@ -9,7 +9,21 @@ const nextConfig = {
       },
     ],
   },
-  transpilePackages: ["@prismicio/next", "@prismicio/client", "@prismicio/react"],
+  async redirects() {
+    return [
+      {
+        source: "/masterclass",
+        destination:
+          "https://docs.google.com/forms/d/1HrEdw3xfPeAsJdvbqKY6SXlEMEEuVK9rGClsjdzRVnA/edit",
+        permanent: true,
+      },
+    ];
+  },
+  transpilePackages: [
+    "@prismicio/next",
+    "@prismicio/client",
+    "@prismicio/react",
+  ],
 };
 
 module.exports = nextConfig;
